04c76d62d4d913521d8f94015929c53432d929d8
Alberto De Pena <alberto.depena@craneww.com>
2026-08-12 11:18:46 -0500

    Minor refactoring

--- internal/adapter/sqlite/txcontext.go
+++ internal/adapter/sqlite/txcontext.go
@@ -5,16 +5,16 @@ import (
 	"database/sql"
 )
 
-type ctxKey struct{}
+type txCtxKey struct{}
 
 // withTx returns a child context carrying the given transaction.
 func withTx(ctx context.Context, tx *sql.Tx) context.Context {
-	return context.WithValue(ctx, ctxKey{}, tx)
+	return context.WithValue(ctx, txCtxKey{}, tx)
 }
 
 // txFromContext retrieves the transaction from context, or nil.
 func txFromContext(ctx context.Context) *sql.Tx {
-	tx, _ := ctx.Value(ctxKey{}).(*sql.Tx)
+	tx, _ := ctx.Value(txCtxKey{}).(*sql.Tx)
 	return tx
 }
 
